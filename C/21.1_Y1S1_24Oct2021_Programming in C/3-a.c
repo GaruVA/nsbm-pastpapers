1e687257e1d37cf579a2ebe7191c9f2aa695e839
Garuka Veenath Assalaarachchi <119990432+GaruVA@users.noreply.github.com>
2023-08-09 20:01:24 +0530

    Update 3-a.c

--- C/21.1_Y1S1_24Oct2021_Programming in C/3-a.c
+++ C/21.1_Y1S1_24Oct2021_Programming in C/3-a.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    for(int i = 1; i <= 5; i++ ) {
-        for(int j = 1; j <= i; j++) {
+    //while
+    int wrow = 1;
+    while(wrow <= 5) {
+        int wcol = 1;
+        while(wcol <= wrow) {
             printf("*");
+            wcol += 1;
         }
-    printf("\n");
+        printf("\n");
+        wrow += 1;
     }
+
+    //do-while
+    int drow = 1;
+    do {
+        int dcol = 1;
+        do {
+            printf("*");
+            dcol += 1;
+        } while(dcol <= drow);
+        printf("\n");
+        drow += 1;
+    } while(drow <= 5);
+    
 }
